6c341c7f48da5a5d0db73c3c2517a69acf4e5f53
Mike <michael_eric_moore@hotmail.com>
2019-07-07 01:17:40 -0500

    added ability to specify custom ranges and allow interlaced/doublescan

--- src/components/monitorConfigurator/monitorConfigurator.js
+++ src/components/monitorConfigurator/monitorConfigurator.js
@@ -14,29 +14,56 @@ export default class MonitorConfigurator {
     this.id = id;
     
     this.elem = htmlToBlock(monitorConfiguratorTemplate).firstElementChild;
-    this.presetInputElem      = this.elem.querySelector('.monitor-configurator__preset-input');
-    this.orientationInputElem = this.elem.querySelector('.monitor-configurator__orientation-input');
+    this.presetInputElem             = this.elem.querySelector('.monitor-configurator__preset-input');
+    this.orientationInputElem        = this.elem.querySelector('.monitor-configurator__orientation-input');
+    this.rangesRowElem               = this.elem.querySelector('.monitor-configurator__ranges-row');
+    this.rangesInputElem             = this.elem.querySelector('.monitor-configurator__ranges-input');
+    this.allowInterlacedCheckboxElem = this.elem.querySelector('.monitor-configurator__allow-interlaced-checkbox');
+    this.allowDoublescanCheckboxElem = this.elem.querySelector('.monitor-configurator__allow-doublescan-checkbox');
+    
+    this.presetInputElem.addEventListener('change', () => {
+      this.updateRangesVisibility();
+    });
   }
   
   init() {
     const modelineConfig = this.loadState();
     if (modelineConfig) {
-      this.presetInputElem     .value = modelineConfig.preset;
-      this.orientationInputElem.value = modelineConfig.orientation;
+      this.presetInputElem            .value   = modelineConfig.preset;
+      this.orientationInputElem       .value   = modelineConfig.orientation;
+      this.rangesInputElem            .value   = modelineConfig.ranges.join('\n');
+      this.allowInterlacedCheckboxElem.checked = modelineConfig.allowInterlaced;
+      this.allowDoublescanCheckboxElem.checked = modelineConfig.allowDoublescan;
     }
+    this.updateRangesVisibility();
   }
   
   /**
    * @returns {ModelineConfig}
    */
   getModelineConfig() {
+    const preset      = this.presetInputElem     .value;
+    const orientation = this.orientationInputElem.value;
+    const ranges = preset !== 'custom'? [] : (
+      this.rangesInputElem.value
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line.length > 0)
+    );
+    const allowInterlaced = this.allowInterlacedCheckboxElem.checked;
+    const allowDoublescan = this.allowDoublescanCheckboxElem.checked;
+    
     return {
-      preset     : this.presetInputElem     .value,
-      orientation: this.orientationInputElem.value,
-      ranges: [],
-      allowDoublescan: true,
-      allowInterlaced: true
+      preset,
+      orientation,
+      ranges,
+      allowInterlaced,
+      allowDoublescan
     };
+  }
+  
+  updateRangesVisibility() {
+    this.rangesRowElem.classList.toggle('hidden', this.presetInputElem.value !== 'custom');
   }
   
   /**
