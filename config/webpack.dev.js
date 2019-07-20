a15abc95f3b0e8639455ebe20b207150ec6d085c
Mike <michael_eric_moore@hotmail.com>
2019-07-20 17:50:38 -0500

    added source maps back

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  //devtool: 'source-map',
+  devtool: 'source-map',
   devServer: {
     contentBase: './dist',
     port: 8080
